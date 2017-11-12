18c22689b9192d7cc3b1b92fa20cf577e38284d4
Alex <ajhansen@mail.usf.edu>
2017-11-12 12:08:47 -0500

    update

--- main.c
+++ main.c
@@ -7,7 +7,7 @@
 #define lo8(x) ( (x) & 0xFF)
 #define hi8(x) ( ( (x) >> 8) & 0xFF)
 #define RAMEND 0x07F7
-#define N_SAMPLES 16
+#define N_SAMPLES 32
 
 complex16 compose_complex(float16, float16);
 float16 convert_float_to_float16(void *);
@@ -29,6 +29,7 @@ void reset()
 
 int main()
 {
+  /*
   unsigned char twiddles[2 * N_SAMPLES];
   twiddles[0] = 0x00;   twiddles[1] = 0x3f;   twiddles[2] = 0x00;   twiddles[3] = 0x00;
   twiddles[4] = 0xd9;   twiddles[5] = 0x3e;   twiddles[6] = 0x87;   twiddles[7] = 0xbd;
@@ -38,6 +39,25 @@ int main()
   twiddles[20] = 0x87;  twiddles[21] = 0xbd;  twiddles[22] = 0xd9;  twiddles[23] = 0xbe;
   twiddles[24] = 0x6a;  twiddles[25] = 0xbe;  twiddles[26] = 0x6a;  twiddles[27] = 0xbe;
   twiddles[28] = 0xd9;  twiddles[29] = 0xbe;  twiddles[30] = 0x87;  twiddles[31] = 0xbd;
+  */
+
+  unsigned char twiddles[64];
+  twiddles[0] = 0x00;	twiddles[1] = 0x3f;	twiddles[2] = 0x00;	twiddles[3] = 0x00;
+  twiddles[4] = 0xf6;	twiddles[5] = 0x3e;	twiddles[6] = 0x8f;	twiddles[7] = 0xbc;
+  twiddles[8] = 0xd9;	twiddles[9] = 0x3e;	twiddles[10] = 0x87;	twiddles[11] = 0xbd;
+  twiddles[12] = 0xa9;	twiddles[13] = 0x3e;	twiddles[14] = 0x1c;	twiddles[15] = 0xbe;
+  twiddles[16] = 0x6a;	twiddles[17] = 0x3e;	twiddles[18] = 0x6a;	twiddles[19] = 0xbe;
+  twiddles[20] = 0x1c;	twiddles[21] = 0x3e;	twiddles[22] = 0xa9;	twiddles[23] = 0xbe;
+  twiddles[24] = 0x87;	twiddles[25] = 0x3d;	twiddles[26] = 0xd9;	twiddles[27] = 0xbe;
+  twiddles[28] = 0x8f;	twiddles[29] = 0x3c;	twiddles[30] = 0xf6;	twiddles[31] = 0xbe;
+  twiddles[32] = 0x00;	twiddles[33] = 0x00;	twiddles[34] = 0x00;	twiddles[35] = 0xbf;
+  twiddles[36] = 0x8f;	twiddles[37] = 0xbc;	twiddles[38] = 0xf6;	twiddles[39] = 0xbe;
+  twiddles[40] = 0x87;	twiddles[41] = 0xbd;	twiddles[42] = 0xd9;	twiddles[43] = 0xbe;
+  twiddles[44] = 0x1c;	twiddles[45] = 0xbe;	twiddles[46] = 0xa9;	twiddles[47] = 0xbe;
+  twiddles[48] = 0x6a;	twiddles[49] = 0xbe;	twiddles[50] = 0x6a;	twiddles[51] = 0xbe;
+  twiddles[52] = 0xa9;	twiddles[53] = 0xbe;	twiddles[54] = 0x1c;	twiddles[55] = 0xbe;
+  twiddles[56] = 0xd9;	twiddles[57] = 0xbe;	twiddles[58] = 0x87;	twiddles[59] = 0xbd;
+  twiddles[60] = 0xf6;	twiddles[61] = 0xbe;	twiddles[62] = 0x8f;	twiddles[63] = 0xbc;
 
   complex16 array[N_SAMPLES];
 
